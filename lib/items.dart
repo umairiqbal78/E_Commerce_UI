754014f8f310487a0e692c702c9efec49210e9e1
Umair-Iqbal <51910760+umairiqbal78@users.noreply.github.com>
2021-07-02 11:47:41 +0500

    updating files

--- lib/items.dart
+++ lib/items.dart
@@ -317,6 +317,30 @@ class _ItemsState extends State<Items> {
             ],
           ),
         ),
+        BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+              backgroundColor: Colors.green,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+              backgroundColor: Colors.purple,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+              backgroundColor: Colors.pink,
+            ),
+          ],
+        ),
       ],
     );
   }
